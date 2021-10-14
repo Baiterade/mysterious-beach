c9b42d02714605e0960a69d6c076435c6f83535d
Jager O <44758792+Baiterade@users.noreply.github.com>
2021-10-13 22:46:25 -0500

    Initial commit

---
+++ Develop/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+const PORT = 5500;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to retrieve notes`);
+    delete require.cache[require.resolve('./db/db.json')];
+    let notesDB = require('./db/db.json');
+    res.json(notesDB);
+});
+
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+        };
+
+        // Obtain existing reviews
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new review
+                parsedNotes.push(newNote);
+
+                // Write updated reviews back to the file
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) => {
+                        return writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!');
+                    }
+                );
+            }
+        });
+    }
+    res.json(req.body);
+});
+
+app.delete('/api/notes/:id', (req, res) => req.json(notesDB));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
