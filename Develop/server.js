377cc8e78fb19574916f24c59be9204ed83efbac
Jager O <44758792+Baiterade@users.noreply.github.com>
2021-10-18 21:10:09 -0500

    fixed routing

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const router = require('./routes/router')
+const fs = require('fs');
 const path = require('path');
-const fs = require('fs')
 
 const PORT = process.env.PORT || 5500;
 
@@ -13,8 +12,14 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use('/', router);
-app.use('/notes', router);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received to retrieve notes`);
